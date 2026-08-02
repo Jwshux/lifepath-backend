26ce47966989ace997f3520ad36737d7c9dc3f0e
Jwshux <joshuariana05@gmail.com>
2026-08-03 03:59:31 +0800

    feat: add admin dashboard statistics endpoint

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 
+from ..extensions import db
 from ..utils.admin import admin_required
 
 
@@ -17,4 +18,33 @@ def get_admin_profile(current_admin):
             'email': current_admin['email'],
             'role': current_admin.get('role', 'user'),
         },
+    }), 200
+
+
+@admin_bp.get('/dashboard')
+@admin_required
+def get_dashboard(current_admin):
+    registered_users = db.users.count_documents({})
+    feedback_received = db.feedback.count_documents({})
+
+    latest_release = db.releases.find_one(
+        sort=[('uploaded_at', -1)]
+    )
+
+    current_version = None
+    latest_upload = None
+
+    if latest_release:
+        current_version = latest_release.get('version')
+
+        uploaded_at = latest_release.get('uploaded_at')
+
+        if uploaded_at:
+            latest_upload = uploaded_at.isoformat()
+
+    return jsonify({
+        'registered_users': registered_users,
+        'feedback_received': feedback_received,
+        'current_version': current_version,
+        'latest_upload': latest_upload,
     }), 200
